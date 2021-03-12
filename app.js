404117f906a73685f983e0b55ab470dd2a084b06
Rishabh Dewari <rdewari07@gmail.com>
2021-03-12 11:14:46 +0530

    fixes issues of register router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const Register = require("./models/userRegistraion");
+const bodyParser = require("body-parser");
 
 const router = express.Router();
 const app = express();
@@ -25,7 +26,8 @@ mongoose
     console.log("No link to database.");
   });
 
-router.post("/Register", async (req, res) => {
+router.post("/user/register", async (req, res) => {
+  console.log(req.body);
   try {
     let firstName = req.body.firstName;
     let secondName = req.body.secondName;
@@ -34,41 +36,38 @@ router.post("/Register", async (req, res) => {
     let confirmPassword = req.body.confirmPassword;
     let email = req.body.email;
     let validatedPassword;
-    let bool = true;
     if (password === confirmPassword) {
-      validatedPassword = bcrypt.hashSync(password, 10);
+      validatedPassword = await bcrypt.hash(password, 10);
     } else {
       throw "password did not match";
     }
-    let users = await Register.find();
-    console.log("users", users);
-    if (users.length >= 1) {
-      await Promise.all(
-        users.map(async (record, index) => {
-          if (record.email === email || record.userName === userName) {
-            throw "username or email is already exist";
-          }
-        })
-      );
+    let userRecord = await Register.findOne({
+      $or: [{ email: email }, { userName: userName }],
+    });
+    if (userRecord) {
+      throw "username or email is already exist";
     }
-    if (bool) {
-      let userData = {
-        firstName: firstName,
-        secondName: secondName,
-        userName: userName,
-        password: validatedPassword,
-        email: email,
-      };
-      let user = new Register(userData);
-      await user.save();
-    }
+
+    let userData = {
+      firstName: firstName,
+      secondName: secondName,
+      userName: userName,
+      password: validatedPassword,
+      email: email,
+    };
+    let user = new Register(userData);
+    await user.save();
+
+    res.status(200).send("registered succefully");
   } catch (error) {
-    res.send({
+    res.status(500).send({
       error: error,
     });
-  }
+  }                                              
 });
 
+app.use(bodyParser.json());
+app.use(router);
 app.listen(3000, () => {
   console.log("connected ");
 });
--- models/userRegistraion.js
+++ models/userRegistraion.js
@@ -22,6 +22,13 @@ const userRegistrationSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    validate: {
+      validator: function (v) {
+        return /^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/.test(v);
+      },
+
+      message: "{VALUE} is not a valid email!",
+    },
   },
 });
 
